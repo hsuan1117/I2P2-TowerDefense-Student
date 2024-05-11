a766643da2d94ab678d2f7fb949dc1ecb5a66368
ablackcat04 <wuru87@gmail.com>
2024-05-11 09:19:58 +0800

    register start scene to game

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ int main(int argc, char **argv) {
 	Engine::GameEngine& game = Engine::GameEngine::GetInstance();
 
     // TODO: [HACKATHON-1-SCENE] (3/4): Register Scenes here
+    game.AddNewScene("start", new StartScene());
 	game.AddNewScene("settings", new SettingsScene());
 	game.AddNewScene("play", new PlayScene());
 	game.AddNewScene("lose", new LoseScene());
@@ -22,7 +23,7 @@ int main(int argc, char **argv) {
 
     // TODO: [HACKATHON-1-SCENE] (4/4): Change the start scene
 //	game.Start("stage-select", 60, 1600, 832);
-	game.Start("settings", 60, 1600, 832);
+	game.Start("start", 60, 1600, 832);
 	return 0;
 }
 //aaaaa
