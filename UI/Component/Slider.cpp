f7353c7d0231c9c144b0c983e78a84a5f20aa0b4
ablackcat04 <wuru87@gmail.com>
2024-05-11 10:02:56 +0800

    draw the setting slider correctly

--- UI/Component/Slider.cpp
+++ UI/Component/Slider.cpp
@@ -15,6 +15,9 @@ Slider::Slider(float x, float y, float w, float h) :
 }
 void Slider::Draw() const {
     // TODO: [HACKATHON-3-BUG] (4/5): Draw the nested components here, so they are displayed correctly
+    Bar.Draw();
+    End1.Draw();
+    End2.Draw();
 	ImageButton::Draw();
 }
 void Slider::SetOnValueChangedCallback(std::function<void(float value)> onValueChangedCallback) {
