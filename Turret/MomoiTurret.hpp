23a9a008e3d270e9e8d081aec5a08cb1e5f524b9
ablackcat04 <wuru87@gmail.com>
2024-05-12 09:28:48 +0800

    create MomoiTurret file

---
+++ Turret/MomoiTurret.hpp
@@ -0,0 +1,14 @@
+//
+// Created by wuru8 on 2024/5/12.
+//
+
+#ifndef INC_2024_I2P2_TOWERDEFENSE_MOMOITURRET_HPP
+#define INC_2024_I2P2_TOWERDEFENSE_MOMOITURRET_HPP
+
+
+class MomoiTurret {
+
+};
+
+
+#endif //INC_2024_I2P2_TOWERDEFENSE_MOMOITURRET_HPP
